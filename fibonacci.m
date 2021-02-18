455c8b54577676a90059a73d9de15a5e436bf48d
= <=>
2021-02-17 22:18:53 -0300

    fixing fibonacci

--- fibonacci.m
+++ fibonacci.m
@@ -1,105 +1,93 @@
-function fibonacci(f, interval, n, is_min)
+function [test_points bounds_history] = fibonacci(f, initial_bounds, n, is_min)
 
     % Valida entrada
     if (n <= 2)
-        fprintf('Ordem inválida! n deve ser >= 2')
+        fprintf('Ordem inválida! n deve ser > 2')
         return
     end
 
-    MAX_ITERATIONS = 10
+    history = zeros(n - 2, 7) % j a b x1 x2 f1 f2
     
     % Inicializa variaveis
-    lower_bound = interval(1)
-    upper_bound = interval(2)
-    displacement = (upper_bound - lower_bound) * (get_fibonacci_number(n - 2) / get_fibonacci_number(n))
+    interval_length = 0
+    is_long_displacement = false
+
+    x1 = 0
+    x2 = 0
     
-    middle_left = lower_bound + displacement
-    middle_right = upper_bound - displacement
-    
-    history = zeros(MAX_ITERATIONS, 7) % j x a x1 x2 b f_x
-    history(1, 1:size(history, 2)) = [1 middle_left lower_bound upper_bound middle_left middle_right f(middle_left)]
-    history(2, 1:size(history, 2)) = [2 middle_right lower_bound upper_bound middle_left middle_right f(middle_right)]
-    
-    new_x = 0
+    f1 = 0
+    f2 = 0
+
+    a = initial_bounds(1)
+    b = initial_bounds(2)
+
     i = 1
     j = 2
+    displacement = get_displacement(n, j, b - a) 
     
     while (j < n)
 
-        % Valida maximo de iteracoes
-        if (i == MAX_ITERATIONS)
-            fprintf('FALHA: Maximo de iterações (%d) atingido\n', MAX_ITERATIONS)
-            return
-        end
+        % Determina pontos para analisar
+        interval_length = b - a
+        is_long_displacement = displacement > (interval_length / 2)
 
-        fl = f(middle_left)
-        fr = f(middle_right)
-        
-        % Atualiza intervalo: Direcao esquerda
-        displacement = middle_right - middle_left
-        
-        if ((is_min && (fl < fr)) || (~is_min && (fl > fr)))
-            
-            upper_bound = middle_right
-            new_x = lower_bound + displacement
-
-            if (new_x < middle_left)
-                aux = middle_left
-                middle_left = new_x
-                middle_right = aux
-            else
-                middle_right = new_x
-            end
-            
-        % Atualiza intervalo: Direcao direita
-        elseif ((is_min && (fl > fr)) || (~is_min && (fl < fr)))
-            
-            lower_bound = middle_left
-            new_x = lower_bound + displacement
-
-            if (new_x > middle_right)
-                aux = middle_right
-                middle_right = new_x
-                middle_left = aux
-            else
-                middle_left = new_x
-            end
-            
-        % Atualiza intervalo: Centro
+        if (is_long_displacement)
+            x1 = b - displacement
+            x2 = a + displacement
         else
+            x1 = a + displacement
+            x2 = b - displacement
+        end
+
+        % Atualiza intervalo de incerteza
+        f1 = f(x1)
+        f2 = f(x2)
+
+        history(i, 1:size(history, 2)) = [j a b x1 x2 f1 f2]
+
+        if (f1 == f2)
             
-            lower_bound = middle_left
-            upper_bound = middle_right
-            displacement = (upper_bound - lower_bound) * (get_fibonacci_number(n - j) / get_fibonacci_number(n - (j - 2)))
-            
-            middle_left = lower_bound + displacement
-            new_x = middle_left
-            history(j, 1:size(history, 2)) = [j new_x lower_bound upper_bound middle_left middle_right f(new_x)]
-            
+            a = x1
+            b = x2
+            displacement = get_displacement(n, j, b - a)
             j = j + 1
-            middle_right = upper_bound - displacement
-            new_x = middle_right
+
+        else
+
+            if ((is_min && (f1 < f2)) || (~is_min && (f1 > f2)))
+                b = x2
+            else
+                a = x1
+            end
+
+            displacement = get_displacement(n, j, interval_length)
 
         end
 
-        % Prepara para proxima iteracao
         j = j + 1
         i = i + 1
-        history(j, 1:size(history, 2)) = [j new_x lower_bound upper_bound middle_left middle_right f(new_x)]
 
     end
 
+    % Determina saida
+    test_points = unique(reshape(history(:, 4), [1, n - 2]), 'stable')
+    bounds_history = unique(reshape(history(:, 2:3), [1, 2 * (n - 2)]), 'stable')
+    
     % Determina ponto otimo
-    optimun_x = (lower_bound + upper_bound) / 2
-    optimun_f_x = f(optimun_x)
+    optimun_x = (a + b) / 2
+    optimun_f = f(optimun_x)
 
     % Exibe resultado
     fprintf('Histórico: ')
     history
 
     fprintf('\nIterações: %d', i)
-    fprintf('\nÓtimo: (%d, %d)', optimun_x, optimun_f_x)
+    fprintf('\nÓtimo: (%d, %d)', optimun_x, optimun_f)
     fprintf('\n-- FIM --\n')
+end
+
+function [displacement] = get_displacement(n, j, interval_length)
+    displacement = interval_length * get_fibonacci_number(n - j) / get_fibonacci_number(n -(j - 2))
 end
 
 
